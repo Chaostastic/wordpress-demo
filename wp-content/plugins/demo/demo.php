9ff59520dec7a98f664b3789bb030046b034c5e6
Chaostastic <chaostastic.dev@gmail.com>
2024-07-29 10:18:05 +0300

    Add Demo Endpoint

--- wp-content/plugins/demo/demo.php
+++ wp-content/plugins/demo/demo.php
@@ -1,14 +1,29 @@
 <?php
 
+/*
+ * Plugin Name: demo
+ */
+
 if (!defined('ABSPATH')) {
 	exit;
 }
 
 if (!class_exists('DemoPlugin')) {
 	class DemoPlugin {
-		public function __construct() {
-			echo 'Hello World!';
-		}
+        public function __construct() {
+            add_action('rest_api_init', array($this, 'register_routes'));
+        }
+
+        function hello_world() {
+            return rest_ensure_response('Hello World!');
+        }
+
+        function register_routes() {
+            register_rest_route('hello-world/v1', '/hello', array(
+                'methods'  => WP_REST_Server::READABLE,
+                'callback' => array($this, 'hello_world'),
+            ));
+        }
 	}
 
 	new DemoPlugin();
