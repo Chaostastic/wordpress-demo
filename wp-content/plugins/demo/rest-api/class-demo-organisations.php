f5c6ef3a56b383a1fdde6c271e8855659d51706f
Chaostastic <chaostastic.dev@gmail.com>
2024-07-31 15:11:00 +0300

    Remove unnecessary queries

--- wp-content/plugins/demo/rest-api/class-demo-organisations.php
+++ wp-content/plugins/demo/rest-api/class-demo-organisations.php
@@ -1,29 +1,26 @@
 <?php
-
 namespace Demo\REST_API;
+use WP_Error;
 
 class Organisations {
     static function post($request) {
         self::add_orgs($request->get_json_params(), null);
     }
 
-    static function add_orgs($arr, $parent_name) {
+    static function add_orgs($arr, $parent_id) {
         global $wpdb;
         $org_name = $arr['org_name'];
         $org_id = self::get_org_id($org_name);
-        if (!self::get_org_id($org_name)) {
+        if (!$org_id) {
             $wpdb->insert($wpdb->prefix . 'demo_organisations', array('orgname' => $org_name));
             $org_id = $wpdb->insert_id;
         }
-        if ($parent_name) {
-            $parent_id = self::get_org_id($parent_name);
-            if ($parent_id) {
-                $wpdb->insert($wpdb->prefix . 'demo_relations', array('parent' => $parent_id, 'child' => $org_id));
-            }
+        if ($parent_id) {
+            $wpdb->insert($wpdb->prefix . 'demo_relations', array('parent' => $parent_id, 'child' => $org_id));
         }
         if (array_key_exists('daughters', $arr)) {
             foreach ($arr['daughters'] as $daughter_arr) {
-                self::add_orgs($daughter_arr, $org_name);
+                self::add_orgs($daughter_arr, $org_id);
             }
         }
     }
